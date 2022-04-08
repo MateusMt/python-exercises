d6092a4416d7b8b3b37611cc2f55356da9c5f5c1
Mateus Martins Teixeira <58996165+MateusMt@users.noreply.github.com>
2022-04-08 16:18:10 -0300

    uploading my first exercises

---
+++ cap-2/ex-cap-2.py
@@ -0,0 +1,21 @@
+#Exercícios
+#2.3
+print("Mateus")
+
+#2.4
+a = 3
+b = 5
+result = 2 * a * 3 * b
+print(result)
+
+#2.5
+a = 2
+b = 3
+c = 4
+print(a+b+c)
+
+#2.6
+salario = 750
+aumento = 5
+print(salario + (salario * aumento / 100))
+
